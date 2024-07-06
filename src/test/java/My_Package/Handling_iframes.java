29d0e5944962144534a7db9ac3ac0f8da8341e92
Shaik-Subhan-Vali <subhanshaik7781@gmail.com>
2024-07-06 18:16:41 +0530

    tests added

---
+++ src/test/java/My_Package/Handling_iframes.java
@@ -0,0 +1,48 @@
+package My_Package;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Handling_iframes {
+	       //declaring driver globally
+			WebDriver driver ;
+			
+			@BeforeTest
+			public void setup() {
+				//creating an instance of chrome driver.
+				driver = new ChromeDriver();
+				driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+				driver.manage().window().maximize();
+			}
+			
+			@Test
+			public void tc1() throws InterruptedException {
+				
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+	          
+				WebElement ifr = driver.findElement(By.id("courses-iframe"));
+				
+				js.executeScript("arguments[0].scrollIntoView(true)",ifr);
+				driver.switchTo().frame(ifr);
+				driver.findElement(By.xpath("(//a[@class=\"theme-btn\"])[1]")).click();	
+				Thread.sleep(2000);
+				driver.switchTo().defaultContent();		
+				
+							
+			}
+			
+			@AfterTest(enabled = false)
+			public void teardwon() throws InterruptedException
+			{
+				Thread.sleep(3000);
+				driver.quit();
+			}
+	
+}
