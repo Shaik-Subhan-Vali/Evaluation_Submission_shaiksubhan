abfb1d40c6adf07e8f15f9c0f2d83fd5b7b2d84d
Shaik-Subhan-Vali <subhanshaik7781@gmail.com>
2024-07-06 16:56:59 +0530

    tests added

--- src/test/java/My_Package/Handling_Alerts.java
+++ src/test/java/My_Package/Handling_Alerts.java
@@ -24,7 +24,26 @@ public class Handling_Alerts {
 		
 		@Test
 		public void tc1() throws InterruptedException {
+          
+			JavascriptExecutor js = (JavascriptExecutor) driver;
 			
+			driver.findElement(By.id("alertBox")).click();
+			Alert A = driver.switchTo().alert();
+			A.accept();
+			
+			js.executeScript("window.scrollBy(0,100) ");
+			js.executeScript("window.scrollBy(0,100) ");
+					
+			Thread.sleep(2000);
+			driver.findElement(By.id("confirmBox")).click();
+			Alert A1 = driver.switchTo().alert();
+			A1.accept();
+			Thread.sleep(2000);
+			driver.findElement(By.id("promptBox")).click();
+			Alert A2 = driver.switchTo().alert();
+			A2.sendKeys("Hello All");
+			A2.accept();
+			Thread.sleep(2000);
 				
 						
 		}
